bb30c4d157634be40a73219d6eea50fdac741c19
unixvextor <withun.wch@gmail.com>
2026-03-22 14:09:04 +0700

    update: add opencode nvim

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -32,3 +32,36 @@ map("n", "<leader>lg", "<cmd>LazyGit<CR>", { desc = "nvimtree focus window" })
 
 -- Telescope ssh
 map({ "n", "v" }, "<leader>fc", "<cmd>Telescope ssh-config<CR>", { desc = "Open an ssh extension" })
+
+map("n", "ca", function()
+  vim.lsp.buf.code_action()
+end, { desc = "LSP Code Action" })
+
+-- Opencode keymaps
+vim.keymap.set({ "n", "x" }, "<C-a>", function()
+  require("opencode").ask("@this: ", { submit = true })
+end, { desc = "Ask opencode…" })
+vim.keymap.set({ "n", "x" }, "<C-x>", function()
+  require("opencode").select()
+end, { desc = "Execute opencode action…" })
+vim.keymap.set({ "n", "t" }, "<C-o>", function()
+  require("opencode").toggle()
+end, { desc = "Toggle opencode" })
+
+vim.keymap.set({ "n", "x" }, "go", function()
+  return require("opencode").operator "@this "
+end, { desc = "Add range to opencode", expr = true })
+vim.keymap.set("n", "goo", function()
+  return require("opencode").operator "@this " .. "_"
+end, { desc = "Add line to opencode", expr = true })
+
+vim.keymap.set("n", "<S-C-u>", function()
+  require("opencode").command "session.half.page.up"
+end, { desc = "Scroll opencode up" })
+vim.keymap.set("n", "<S-C-d>", function()
+  require("opencode").command "session.half.page.down"
+end, { desc = "Scroll opencode down" })
+
+-- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
+vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
